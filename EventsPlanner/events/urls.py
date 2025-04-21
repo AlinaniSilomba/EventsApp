55c7ee2aebc3159ac15b460f9b0b37ba7c1f1dc7
Alinani <alinanisilomba1@gmail.com>
2025-04-21 10:17:25 +0200

    Add authentication URLs and enhance templates with navigation and styling
    - Set LOGIN_REDIRECT_URL and LOGOUT_REDIRECT_URL in settings.py
    - Update urls.py to include index path
    - Enhance index.html with a navigation bar and links
    - Improve sign_up.html and login.html with better form styling and links
    - Add CSS for styling forms and navigation
    - Create app.js for future JavaScript functionality

--- EventsPlanner/events/urls.py
+++ EventsPlanner/events/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), 
+    path('index', views.index, name='index'), 
     path('accounts/sign-up/', views.sign_up, name='sign_up'), 
 ]
--- EventsPlanner/events/views.py
+++ EventsPlanner/events/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from.forms import RegistrationForm
-
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
@@ -13,8 +13,14 @@ def index(request):
 def sign_up(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return HttpResponseRedirect(reverse('index'))
     else:
         form = RegistrationForm()
     return render(request,'events/sign_up.html',{
         'form':form
     })
+    
+
