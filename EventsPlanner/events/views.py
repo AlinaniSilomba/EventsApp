fb4e8bb55f50636a4427cc9e7f7b48fcb3240564
Alinani <alinanisilomba1@gmail.com>
2025-04-21 08:41:45 +0200

    Implement user registration and login functionality with forms and templates

--- EventsPlanner/events/views.py
+++ EventsPlanner/events/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from.forms import RegistrationForm
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
     return render(request, 'events/index.html')
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+    else:
+        form = RegistrationForm()
+    return render(request,'events/sign_up.html',{
+        'form':form
+    })
