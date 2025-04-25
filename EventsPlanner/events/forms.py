12ac516ee9798ecda2e348ec9be91cb1c7091ccd
Alinani <alinanisilomba1@gmail.com>
2025-04-25 06:44:40 +0200

    Refactor forms, templates, and styles for improved layout and user experience; update field order in RegistrationForm, enhance index and logout templates, and add new CSS styles for better visual consistency.

--- EventsPlanner/events/forms.py
+++ EventsPlanner/events/forms.py
@@ -6,6 +6,6 @@ class RegistrationForm(UserCreationForm):
     email = forms.EmailField(label="Email",required=True)
     class Meta:
         model = User
-        fields = ['email','username','password1','password2',]
+        fields = ['username', 'email','password1','password2',]
         
     
